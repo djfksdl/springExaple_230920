9f79b7d60f3db80002b3c00e745aa4102b067a65
학원 <test@test.com>
2023-09-21 21:45:44 +0900

    230921

--- src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
+++ src/main/java/com/example/lesson02/bo/UsedGoodsBO.java
@@ -1,13 +1,24 @@
 package com.example.lesson02.bo;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.lesson02.domain.UsedGoods;
+import com.example.lesson02.mapper.UsedGoodsMapper;
 
 @Service   //spring bean
 public class UsedGoodsBO {
+	
+	
+	@Autowired  //spring bean(객체) 가져오는것 - DI(Dependency Injection)
+	private UsedGoodsMapper usedGoodsMapper;
+	
 	// input (컨트롤러로부터 요청받음) : x
 	// output (컨트롤러한테 돌려줌) : List<UsedGoods>
 	public List<UsedGoods> getUsedGoodsList(){
-		
+		return usedGoodsMapper.selectUsedGoodsList();
 	}
 	
 }
---
+++ src/main/java/com/example/lesson03/lesson03Ex01RestController.java
@@ -0,0 +1,13 @@
+package com.example.lesson03;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class lesson03Ex01RestController {
+	//요청 url : http://localhost/lesson03/ex01
+	@RequestMapping("/lesson03/ex01")
+		public Review ex01() {
+		 return ;
+	}
+}
