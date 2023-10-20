fb0bd15a3d21c950faf4bde104e32c02a04e3376
학원 <test@test.com>
2023-10-20 21:41:01 +0900

    1020

--- src/main/java/com/example/lesson04/bo/StudentBO.java
+++ src/main/java/com/example/lesson04/bo/StudentBO.java
@@ -1,17 +1,25 @@
 package com.example.lesson04.bo;
 
+import java.time.ZonedDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.lesson04.domain.Student;
 import com.example.lesson04.mapper.StudentMapper;
+import com.example.lesson07.entity.StudentEntity;
+import com.example.lesson07.repository.StudentRepository;
 
 @Service
 public class StudentBO {
 
 	@Autowired
-	private StudentMapper studentMapper;
+	private StudentMapper studentMapper;//mybatis
 	
+	@Autowired
+	private StudentRepository studentRepository;  //JPA
+	
+	//mybatis
 	public void addStudent(Student student) {
 		studentMapper.insertStudent(student);
 	}
@@ -19,6 +27,21 @@ public class StudentBO {
 	public Student getStudentById(int id) {
 		return studentMapper.selectStudentById(id);
 	}
+	
+	//JPA
+	//input:파라미터들 output:StudentEntity
+	public StudentEntity addStudent(String name, String phoneNumber , String email, String dreamJob) {
+		StudentEntity student = StudentEntity.builder()
+				.name(name)
+				.phonNumber(phoneNumber)
+				.email(email)
+				.dreamJob(dreamJob)
+				.createdAt(ZonedDateTime.now())  //@UpdateTimestamp있으면 생략 가능
+				.build();
+		
+		return studentRepository.save(student);
+	}
+	
 }
 
 
